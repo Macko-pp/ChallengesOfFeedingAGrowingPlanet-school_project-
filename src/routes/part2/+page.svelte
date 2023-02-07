5b17a23921fd7adfc10f9241d6f48364428df328
Macko++ <mackomaxo@duck.com>
2023-02-06 23:14:45 -0500

    Started p2 until p2bii

--- src/routes/part2/+page.svelte
+++ src/routes/part2/+page.svelte
@@ -1,1 +1,117 @@
-<h1 class="title">Part2</h1>
+<div class="prose">
+
+<h1>Part 2: The Brazilian Rainforest</h1>
+<p>
+	Approximately 70% of clearcutting (cutting down all or most
+	trees in an area) in the Brazilian rainforest is to provide land for
+	cattle ranches and farming. This accounts for almost 15% of the
+	world's total annual deforestation, making it the largest cause of
+	deforestation worldwide.
+	The graph below represents the forest cover in the Brazilian
+	Rainforest since 1970.
+</p>
+<div class="bg-[#7FD87B] rounded-xl p-6 flex justify-center">
+	<img src="/p2.png" alt="the initial graph of the main question" class="h-80">
+</div>
+
+<ol type="a">
+	
+<li class="prose">
+	<p>
+		Determine a linear equation that represents the relationship between the amount of forest cover in the Brazilian Amazon (y), and the year after 1970 (x)
+	</p>
+
+		<p class="ml-10">
+			<b>y = -0.8x + 4,000,000</b>
+				<br> <br>
+			This is because thanks to the graph shown above, I am able to deduce the slope (<b>-0.8</b>) and thereby be able to generate the equation, with the <b>y-intercept</b> being the forest cover in the Brazilian Amazon and x being the number of years after 1970
+		</p>
+</li> <br>
+
+<li class="prose">
+	<ol type="i">
+		<li class="prose">
+			<p>
+				What does the gradient actually mean in this context of <b>deforestation</b>?
+			</p>
+
+				<p class="ml-10">
+					In this context, the gradient or slope means how fast the area the forest is currently occupying is diminishing. Or in other words, <b>how fast we are cutting it down</b> to make space for cattle, livestock or crop fields. It also means that, according to tre calculations, <b>the forest will completely disappear by 2033</b> if we don't stop or at least slow down.
+				</p>
+		</li> <br>
+
+		<li class="prose">
+			<p>
+				What does the <b>y-intercept</b> represent?
+			</p>
+
+				<p class="ml-10">
+					The <b>y-intercept</b> represents the amount of area in m<sup>2</sup> that the trees in the brazilian rainforest are currently occupying.
+				</p>
+		</li>
+	</ol>
+</li> <br>
+
+<li class="prose">
+	<p>
+		To put this area in perspective, <b>research the area</b> of the city or town you live in, in square kilometers. <b>How does it compare</b> with the area of the rainforest that is cleared each year?
+	</p>
+
+		<p class="ml-10">
+			Dolore proident mollit magna ad. Commodo laboris sint id non anim consectetur sit est minim nulla officia amet. Dolor minim aliqua est occaecat. Occaecat non exercitation irure sunt consectetur adipisicing eiusmod mollit do. Fugiat ea ea culpa velit aute minim. Ad do officia tempor id veniam deserunt ut. Labore qui tempor adipisicing consequat non reprehenderit aute non veniam elit.
+		</p>
+</li> <br>
+
+<li class="prose">
+	<p>
+		Given the calculated rate of area clearcut each year (your answer in part a), how long would it take to clearcut an area of the rainforest that is the size of your town or city?
+	</p>
+	
+		<p class="ml-10">
+			Nisi proident velit elit cupidatat qui adipisicing ipsum in ut non nisi consectetur dolor minim. Reprehenderit labore laborum laboris aute incididunt esse aliquip laborum dolor labore enim sit. Est est ex minim magna.
+		</p>
+</li> <br>
+
+<li class="prose">
+	<p>
+		Some scientists warn that the rainforest cannot deplete in size to less than 70% of its 1970 size without catastrophic irreversible consequences to the rainforest ecosystem.<br> 
+		If clearcutting continues at the constant rate represented in the graph, in what year would that occur? 
+	</p>
+
+		<p class="ml-">
+			Magna voluptate exercitation aliquip adipisicing cupidatat nisi sunt id laboris incididunt magna do sunt. Qui consectetur ex excepteur sunt eiusmod nostrud tempor sunt magna sit aliquip labore amet commodo. Aliqua consequat quis ad anim ad qui ipsum excepteur occaecat mollit ut officia consectetur. Duis aute ipsum labore mollit laborum sunt ea.
+		</p>10	
+</li> <br>
+
+<li class="prose">
+	<p>
+		Research and include two reasons FOR and two reasons AGAINST the deforestation of the Brazilian rainforest
+	</p>
+	
+		<p class="ml-10">
+			Aliquip proident aute sunt exercitation do nostrud est nulla anim duis amet ex. Fugiat ullamco non est aliqua est laboris et labore voluptate non enim enim adipisicing. Occaecat fugiat commodo enim tempor ea dolor ipsum. Consequat consequat Lorem officia nostrud eiusmod nisi. Nisi ullamco aliquip aliquip commodo labore cillum reprehenderit pariatur reprehenderit dolore eiusmod tempor ipsum velit. Do nostrud elit quis labore mollit qui exercitation et.
+		</p>
+</li> <br>
+
+</ol>
+
+<div class="prose">
+	<h2><b>End Goal:</b></h2>
+	<p>
+		Determine and explain when meat consumption and deforestation would be unsustainable.
+	</p>
+	
+		<p class="ml-10">
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil fugit itaque, beatae nesciunt ex a exercitationem. Aliquam doloribus sapiente repellat, obcaecati voluptates facilis reiciendis minus ipsum omnis eveniet mollitia saepe.
+		</p>
+</div> <br>
+
+</div>
+
+<style>
+
+	b {
+		color: #7FD87B;
+	}
+
+</style>
