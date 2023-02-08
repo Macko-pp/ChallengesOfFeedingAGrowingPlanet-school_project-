2b4757ef6c35e4841c157f45c49ac55af1591c6c
Macko++ <mackomaxo@duck.com>
2023-02-07 22:24:37 -0500

    Finished EVERYTHING! I'm DONE! *Finally*

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+	import { browser } from '$app/environment';
+ 
+	let isDarkMode: any;
+
+	if (browser) {
+		let mediaQueryObj = window.matchMedia('(prefers-color-scheme: dark)');
+		isDarkMode = mediaQueryObj.matches;
+	}
+
+	console.log(isDarkMode);
+
+</script>
+
+<div class="hero min-h-screen">
+  <div class="hero-content text-center">
+    <div class="max-w-md prose">
+      <h1 class="text-5xl font-bold mt-[-300px]">Challenges of Feeding a Growing Planet</h1>
+      <div class="flex justify-between w-[1000px] ml-[-290px]">
+		<a class="flex flex-col w-96" href="part1">
+			<h2>Part 1</h2>
+			{#if isDarkMode}
+				<img src="/p1Dark.png" alt="Screenshot of Part 1" class="w-96">
+			{:else}
+				<img src="/p1Light.png" alt="Screenshot of Part 1" class="w-96">
+			{/if}
+		</a>
+		<a class="flex flex-col w-96" href="part2">
+			<h2>Part 2</h2>
+			{#if isDarkMode}
+				<img src="/p2Dark.png" alt="Screenshot of Part 2" class="w-96">
+			{:else}
+				<img src="/p2Light.png" alt="Screenshot of Part 2" class="w-96">
+			{/if}
+		</a>
+	  </div>
+    </div>
+  </div>
+</div>
+
+<style>
+
+		img {
+		border-color: #7FD87B;
+		border-width: 3px;
+		border-radius: 1rem;
+	}
+
+</style>
